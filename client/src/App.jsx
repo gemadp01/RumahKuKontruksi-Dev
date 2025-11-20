ba735f39037dfe36349f5c111b9ffad27dfd89a6
syahputrawork98-sketch <syah.putrawork98@gmail.com>
2025-11-20 16:53:06 +0700

    perbaikan

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // ===== LAYOUTS =====
-import MainLayout from "./layouts/MainLayout";
-import KonsumenLayout from "./layouts/KonsumenLayout";
-import SuperadminLayout from "./layouts/SuperAdminLayout";
+
+
+
 
 // ===== HALAMAN UMUM =====
+import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import About from "./pages/Tentang";
 import Contact from "./pages/Contact";
 
 // ===== HALAMAN KONSUMEN =====
+import KonsumenLayout from "./layouts/KonsumenLayout";
 import TimelineProyek from "./pages/konsumen/TimelineProyek";
 import Proyek from "./pages/konsumen/Proyek";
 import Profil from "./pages/konsumen/Profil";
 
 // ===== HALAMAN SUPER ADMIN =====
+import SuperadminLayout from "./layouts/SuperAdminLayout";
 import DashboardSuperadmin from "./pages/superadmin/DashboardSuperadmin";
 import DataAdmin from "./pages/superadmin/DataAdminPage";
 import DataPengawas from "./pages/superadmin/DataPengawasPage";
